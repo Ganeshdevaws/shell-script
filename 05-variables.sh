ca42fd5892238685f0feeac6956fb44e916c8943
ganesh <ganeshjanapareddy2019@gmail.com>
2023-09-07 00:33:33 +0530

    code occu

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,3 +5,9 @@ echo "Please enter your username"
 read USERNAME # this is the variable where the value of the one user entered
 
 echo "username entered is: $USERNAME"
+
+echo "please enter your password"
+
+read PASSWORD
+
+echo "password entered is: PASSWORD"
