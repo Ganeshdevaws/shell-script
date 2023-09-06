10c4019751acd8263a024f093ca1be7c2db99133
ganesh <ganeshjanapareddy2019@gmail.com>
2023-09-06 22:22:27 +0530

    code added

--- 01-hello world.sh
+++ 01-hello world.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 echo "Hello world. I am learning Devops with AWS"
+
+echo "Ramesh: Hi suresh, Good morning"
+echo "Suresh: Good morning Ramesh"
+echo "Ramesh: How are you Suresh"
+echo "Suresh: I am fine, how are you Ramesh"
