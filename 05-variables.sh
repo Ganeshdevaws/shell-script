a591a0cf995de1cd6b93a4b646d8afb6b97edfd0
ganesh <ganeshjanapareddy2019@gmail.com>
2023-09-07 00:51:01 +0530

    code occu

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,6 +4,7 @@ echo "Please enter your username"
 
 read -s USERNAME # this is the variable where the value of the one user entered
 
+#this is only for varition, we should not print inn the terminals
 echo "username entered is: $USERNAME"
 
 echo "please enter your password"
