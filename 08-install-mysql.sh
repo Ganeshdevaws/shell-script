4f53f2856974565e2cf78495cc95ced62b899a99
ganesh <ganeshjanapareddy2019@gmail.com>
2023-09-07 22:08:51 +0530

    added code

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -9,7 +9,7 @@ then
    echo "ERROR:: Please run this script with root access"
    exit 1
 # else
-     echo "INFO:: You are root user"
+#     echo "INFO:: You are root user"
 fi
 
 yum install mysql -y
