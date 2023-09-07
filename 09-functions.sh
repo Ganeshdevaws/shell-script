68fc85b792b27546ba601ca0b7569937c75f2d83
ganesh <ganeshjanapareddy2019@gmail.com>
2023-09-08 00:33:31 +0530

    added code

--- 09-functions.sh
+++ 09-functions.sh
@@ -2,7 +2,10 @@
 
 # our program goal is to install mysql
 
-USERID=$(id -u)
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
 # this function should validate the previous command and inform user it is success or failure
 VALIDATE(){
    #$1 --> it will receive the argument1
@@ -15,6 +18,8 @@ VALIDATE(){
    fi
 }
 
+USERID=$(id -u)
+
 if [ $USERID -ne 0 ]
 then
    echo "ERROR:: Please run this script with root access"
@@ -24,10 +29,10 @@ then
 fi
 
 # it is our responsibility again to check installation is success or not
-yum install mysql -y
+yum install mysql -y %>>$LOGFILE
 
 VALIDATE $? "installing mysql"
 
-yum install postfix -y
+yum install postfix -y %>>$LOGFILE
 
 VALIDATE $? "installing postfix"
